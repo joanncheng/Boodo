6761a0a1427fc5c468efc2c011f2c9cc754bdb1d
joanncheng <antinsss@gmail.com>
2022-06-12 14:08:48 +0800

    refactor: collabBtn component

--- src/components/CollabBtn.js
+++ src/components/CollabBtn.js
@@ -1,13 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { ref, onValue } from 'firebase/database';
+import * as S from './Modal/Modal.styled';
 import { db } from '../firebase';
-import CollabModal from './Modal/CollabModal';
+import Modal from './Modal';
 import CollaborationIcon from '../../public/images/icons/collaboration.svg';
 import BottomToolbarBtn from './BottomToolbarBtn';
+import { displayName } from '../redux/displayName';
 
 const CollabBtn = ({ currentBoard }) => {
   const [modalOpen, setModalOpen] = useState(false);
   const [collaborators, setCollaborators] = useState([]);
+  const username = useSelector(state => state.cursorDisplayName);
+  const inputRef = useRef();
+  const dispatch = useDispatch();
+
+  const copyToClipboard = () => {
+    inputRef.current.focus();
+    inputRef.current.select();
+    navigator.clipboard.writeText(inputRef.current.value);
+  };
 
   useEffect(() => {
     onValue(ref(db, `status/`), snapshot => {
@@ -20,9 +32,38 @@ const CollabBtn = ({ currentBoard }) => {
     });
   }, []);
 
+  const modalActions = (
+    <S.ActionContainer>
+      <S.CopyButton title="Copy" onClick={copyToClipboard}>
+        <S.CopyIcon />
+      </S.CopyButton>
+      <S.TextInput
+        ref={inputRef}
+        type="text"
+        readOnly
+        value={window.location.href}
+        onClick={copyToClipboard}
+      />
+      <label htmlFor="username">Your name</label>
+      <S.TextInput
+        type="text"
+        id="username"
+        value={username}
+        onChange={e => dispatch(displayName(e.target.value))}
+      />
+    </S.ActionContainer>
+  );
+
   return (
     <>
-      {modalOpen ? <CollabModal onDismiss={() => setModalOpen(false)} /> : null}
+      {modalOpen ? (
+        <Modal
+          title="Live collaboration"
+          content="Share this link with anyone you want to collaborate with:"
+          onDismiss={() => setModalOpen(false)}
+          modalActions={modalActions}
+        />
+      ) : null}
       <BottomToolbarBtn
         title="Live collaboration"
         handler={() => setModalOpen(true)}
